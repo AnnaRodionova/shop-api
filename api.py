f54efd0cec9b100b5602dfc2941323e986b6744c
AnnaRodionova <aarodi101@gmail.com>
2019-04-01 00:53:27 +0300

    feat(api): change entities routing

--- api.py
+++ api.py
@@ -1,12 +1,17 @@
-from tornado.web import Application
+from tornado.routing import RuleRouter, Rule, PathMatches
 
-from handlers.companies import CompaniesHandler
-from handlers.employees import EmployeesHandler
-from handlers.products import ProductsHandler
+from applications.companies import make_app as make_companies_app
+from applications.employees import make_app as make_employees_app
+from applications.products import make_app as make_products_app
 
 
 def make_api():
-    return Application([(r"/api/companies", CompaniesHandler),
-                        (r"/api/employees", EmployeesHandler),
-                        (r"/api/products", ProductsHandler)],
-                       debug=True)
+    companies_app = make_companies_app()
+    employees_app = make_employees_app()
+    products_app = make_products_app()
+
+    return RuleRouter([
+        Rule(PathMatches('/api/companies.*'), companies_app),
+        Rule(PathMatches('/api/employees.*'), employees_app),
+        Rule(PathMatches('/api/products.*'), products_app),
+    ])
